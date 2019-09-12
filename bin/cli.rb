2855c249ab5bbebcc83fe6525b0486db5c144282
nicklunn325 <nrlunn325@gmail.com>
2019-09-12 11:19:58 -0400

    fixed some bugs

--- bin/cli.rb
+++ bin/cli.rb
@@ -10,12 +10,13 @@ class Cli
     puts font.write("Welcome to Tweeter")
   end
 
-  def main_menu
+  def run
     prompt = TTY::Prompt.new
-    choice = prompt.select("Would you like to sign in with your username or do you need to create an account?", ["Sign in", "Create an account"])
-    if choice == "Sign in"
+    choice = prompt.select("Would you like to sign in with your username or do you need to create an account?", ["Sign in", "Create an account", "Exit"])
+    case choice
+    when "Sign in"
       user_name = prompt.ask("What is your username?")
-    else
+    when "Create an account"
       full_name = prompt.ask("What is your full name?")
       user_name = prompt.ask("What would you like your username to be?")
       # DONE: see if username already exists, and if so, let user know that username is taken and ask for a different one
@@ -25,14 +26,17 @@ class Cli
       else
         User.create(name: full_name, username: user_name)
       end
+    when "Exit"
+        exit
     end
     user = User.find_by(username: user_name)
     menu_options(user)
+
   end
 
   def menu_options(user)
     prompt = TTY::Prompt.new
-    menu_choice = prompt.select("Hello #{user.name}! What would you like to do?", ["Tweets", "Follows", "Topics", "Exit"]) 
+    menu_choice = prompt.select("Hello #{user.name}! What would you like to do?", ["Tweets", "Follows", "Topics", "Logout", "Exit"]) 
     case menu_choice
       when "Tweets"
         tweet_options(user)
@@ -40,6 +44,8 @@ class Cli
         follow_options(user)
       when "Topics"
         topic_options(user)
+      when "Logout"
+        self.run
       when "Exit" 
         # binding.pry
         exit
@@ -47,22 +53,55 @@ class Cli
     return self.menu_options(user)
   end
 
-  def topic_options(user)
+  def tweet_options(user)
     prompt = TTY::Prompt.new
-    menu_choice = prompt.select("Hell #{user.name}! What would you like to do?", ["See all topics", "See most popular topic", "See all tweets for a topic", "Exit"])
+    menu_choice = prompt.select("Howdy #{user.name}! What would you like to do?", ["Create a new tweet", "See all your tweets", "See all tweets for a topic", "Update a tweet", "Delete a tweet", "Exit"])
     case menu_choice
-    when "See all topics" 
-      Topic.all.each {|topic| puts topic.name.upcase; puts "**********"}
-    when "See most popular topic" 
-      puts Topic.most_popular_topic.upcase
+    when "Create a new tweet" 
+      content = prompt.ask("What should your tweet say?"); 
+      cat = prompt.ask("What topic is your tweet?")
+      tw= Tweet.create(user_id: user.id, message: content)
+      # DONE: see if topic already exists, if not then create new topic, and associate that topic with your tweet 
+      to = Topic.find_or_create_by(name: cat)
+      TweetTopic.create(tweet_id:tw.id, topic_id:to.id)
+    all_topics = Topic.all  
+    when "See all your tweets" 
+      if user.tweets.empty?
+        puts "You do not have any tweets to view."
+          return self.tweet_options(user)
+      else
+      user = User.find(user.id)
+      user.tweets.each {|tweet| puts tweet.message; puts "******************************************"}
+      end
     when "See all tweets for a topic" 
       which_topic = prompt.select("Which topic?", Topic.all.map(&:name))
       topic = Topic.find_by(name: which_topic)
       topic.tweets.map(&:message).each {|x| puts x; puts "******************************************"}  
+    when "Update a tweet" 
+      # DONE:
+      if user.tweets.empty?
+        puts "Sorry, you do not have any tweets to update."
+          return self.tweet_options(user)
+      else
+      which_tweet = prompt.select("Which tweet would you like to update?", user.tweets.map(&:message))
+      tweet = Tweet.find_by(message: which_tweet)
+      update = prompt.ask("What would like to update it to say?")
+      tweet.update(message: update)
+      end
+    when "Delete a tweet" 
+      # DONE: kinda works, but only after closing cli and reopening
+      if user.tweets.empty?
+        puts "Sorry, you do not have any tweets to delete."
+          return self.tweet_options(user)
+      else
+      which_tweet = prompt.select("Which tweet would you like to delete?", user.tweets.map(&:message))
+      tweet = Tweet.find_by(message: which_tweet)
+      tweet.delete
+      end
     when "Exit"
-      return self.menu_options(user)
+        return self.menu_options(user)
+    end
   end
-
 
   def follow_options(user)
     prompt = TTY::Prompt.new
@@ -131,57 +170,23 @@ class Cli
       end
     when "Exit"
       return self.menu_options(user)
+    end
   end
 
-  def tweet_options(user)
+def topic_options(user)
     prompt = TTY::Prompt.new
-    menu_choice = prompt.select("Howdy #{user.name}! What would you like to do?", ["Create a new tweet", "See all your tweets", "See all tweets for a topic", "Update a tweet", "Delete a tweet", "Exit"])
+    menu_choice = prompt.select("Hell #{user.name}! What would you like to do?", ["See all topics", "See most popular topic", "See all tweets for a topic", "Exit"])
     case menu_choice
-    when "Create a new tweet" 
-      content = prompt.ask("What should your tweet say?"); 
-      cat = prompt.ask("What topic is your tweet?")
-      tw= Tweet.create(user_id: user.id, message: content)
-      # DONE: see if topic already exists, if not then create new topic, and associate that topic with your tweet 
-      to = Topic.find_or_create_by(name: cat)
-      TweetTopic.create(tweet_id:tw.id, topic_id:to.id)
-    all_topics = Topic.all  
-    when "See all your tweets" 
-      if user.tweets.empty?
-        puts "You do not have any tweets to view."
-          return self.menu_options(user)
-        else
-      user = User.find(user.id)
-      user.tweets.each {|tweet| puts tweet.message; puts "******************************************"}
-        end
+    when "See all topics" 
+      Topic.all.each {|topic| puts topic.name.upcase; puts "**********"}
+    when "See most popular topic" 
+      puts Topic.most_popular_topic.upcase
     when "See all tweets for a topic" 
       which_topic = prompt.select("Which topic?", Topic.all.map(&:name))
       topic = Topic.find_by(name: which_topic)
       topic.tweets.map(&:message).each {|x| puts x; puts "******************************************"}  
-    when "Update a tweet" 
-      # DONE:
-      if user.tweets.empty?
-        puts "Sorry, you do not have any tweets to update."
-          return self.menu_options(user)
-        else
-      which_tweet = prompt.select("Which tweet would you like to update?", user.tweets.map(&:message))
-      tweet = Tweet.find_by(message: which_tweet)
-      update = prompt.ask("What would like to update it to say?")
-      tweet.update(message: update)
-        end
-    when "Delete a tweet" 
-      # DONE: kinda works, but only after closing cli and reopening
-      if user.tweets.empty?
-        puts "Sorry, you do not have any tweets to delete."
-          return self.menu_options(user)
-        else
-      which_tweet = prompt.select("Which tweet would you like to delete?", user.tweets.map(&:message))
-      tweet = Tweet.find_by(message: which_tweet)
-      tweet.delete
-        end
-      when "Exit"
-        return self.menu_options(user)
-      end
+    when "Exit"
+      return self.menu_options(user)
     end
   end
-end
 end
